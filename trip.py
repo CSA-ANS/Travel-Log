6a2113c0f0d5371bccbbfedb3dc2dac75890f679
CSA-ANS <33459799+CSA-ANS@users.noreply.github.com>
2018-10-24 08:56:20 -0500

    Update trip.py
    I added a character max on the text box, commented the program, and changed widget and window sizes

--- trip.py
+++ trip.py
@@ -3,18 +3,16 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 #####
 Ammaar Siddiqui
 Trip Program
-Version 1 .0
+Version 1.0.1
 This is my trip program
 The user can input their country, description, month, and transport for their vacation.
 They can then save this and view info about the program
@@ -39,7 +37,7 @@ class App():
         helpmenu = Menu(menubar, tearoff=0)
         helpmenu.add_command(label="About", command=self.about_top)
         menubar.add_cascade(label="Help", menu=helpmenu)
-        root.config(menu=menubar)
+        root.config(menu=menubar)#Makes menu with all menubars and seperators, also configs it to the root
 
         content = Frame(root)
         self.country_label = Label(content, text="Country")
@@ -51,18 +49,17 @@ class App():
             self.lbox.insert(END, c)
         self.scroll = ttk.Scrollbar(content, orient=VERTICAL, command=self.lbox.yview)
         self.lbox.configure(yscrollcommand=self.scroll.set)
-        self.descrip_label = Label(content, text="Description")
-        self.descrip_box = Text(content, width=10, height=10, wrap=WORD)
+        self.descrip_label = Label(content, text="Description"+"\n"+"(Only 1,000 characters)")
+        self.descrip_box = Text(content, width=14, height=10, wrap=WORD)
         self.month_label = Label(content, text="Month")
-        self.month_spin = Spinbox(content, textvariable=month,
-                                  values=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-                                          'September', 'October', 'November', 'December'], state='readonly', wrap=True)
+        self.month_spin = Spinbox(content, textvariable=month,values=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December'], state='readonly', wrap=True)
         self.travel_label = Label(content, text="Travel Type")
-        self.travel_cbox = ttk.Combobox(content, values=['Air', 'Train', 'Car', 'Bus'], textvariable=travel,
-                                        state='readonly')
+        self.travel_cbox = ttk.Combobox(content, values=['Air', 'Train', 'Car', 'Bus'], textvariable=travel,state='readonly')
         travel.set("Method of Travel")
         self.travel_cbox.bind('<FocusIn>', self.defocus)
-        self.error_label = Label(content, text="")
+        self.error_label = Label(content, text="")#Makes all widgets and assigns variables to each
+        self.text_error_label=Label(content, text="")
 
         grip_frame = Frame(root)
         self.grip = ttk.Sizegrip(grip_frame).grid(column=999, row=999, sticky='nsew')
@@ -81,24 +78,25 @@ class App():
         self.month_spin.grid(column=3, row=3)
         self.submit.grid(column=3, row=4)
         self.clear.grid(column=3, row=5)
-        self.error_label.grid(column=3, row=6)
+        self.error_label.grid(column=3, row=6)#grids all widgets
+        self.text_error_label.grid(column=2, row=6)
 
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         grip_frame.columnconfigure(999, weight=1)
-        grip_frame.rowconfigure(999, weight=1)
+        grip_frame.rowconfigure(999, weight=1)#weights the main root so it stays in middle when resizing
 
-        root.protocol("WM_DELETE_WINDOW", self.close_window)
+        root.protocol("WM_DELETE_WINDOW", self.close_window)#runs a confirm close when they try and exit the window
 
     def about_top(self):
         explain = Toplevel(root)
         explain.title("About Program")
         explain.geometry("300x100")
         explain.resizable(width=False, height=False)
-        about_label = Label(explain, text=("Trip Program" + "\n" + "Version 1.0" + "\n" + "Ammaar Siddiqui"))
+        about_label = Label(explain, text=("Trip Program" + "\n" + "Version 1.0.1" + "\n" + "Ammaar Siddiqui"))
         about_label.pack()
         button = Button(explain, text="Close", command=explain.destroy)
-        button.pack()
+        button.pack()#Creates a top level and assigns a label and button to it
 
 
     def close_window(self):
@@ -117,50 +115,57 @@ class App():
         spacer_label3 = Label(button_frame, text="")
         spacer_label3.pack(side=LEFT)
         no = Button(button_frame, text="No", command=confirm.destroy)
-        no.pack(side=LEFT)
+        no.pack(side=LEFT)#confirm close top level asks if they are sure they want to close
 
     def clear_all(self):
         travel.set('Method of Travel')
         self.lbox.bind(self.lbox.selection_clear(0, END))
         self.descrip_box.delete("1.0", END)
         month.set("January")
-        self.error_label.config(text="")
+        self.error_label.config(text="")#makes every widget empty
+        self.text_error_label.config(text="")
 
     def defocus(self, event):
-        event.widget.master.focus_set()
+        event.widget.master.focus_set()#gets rid of blue higlighting
 
     def save_all(self):
         check=True
         self.error_label.config(text="")
         values = [self.lbox.get(idx) for idx in self.lbox.curselection()]
         travel_type = travel.get()
-        input = self.descrip_box.get("1.0", END)
+        input = self.descrip_box.get("1.0", END)#grabs all values
+        input=input.rstrip("\n")
         input = input.replace("\n", "*")
-        input=input.replace(" ", "^")
+        input=input.replace(" ", "^")#replaces spaces and new lines
+        input_nospace=input.replace("^", "")
+        input_noline=input_nospace.replace("*", "")#replaces spaces and new lines to check character length
         for x in input:
             if x!="*" and x!="^":
-                check=False
-        if values == [] or travel_type == "Method of Travel" or check:
-            self.error_label.config(text="Error")
+                check=False#checks if something other than sapces and new lines is entered
+        if values == [] or travel_type == "Method of Travel" or check or len(input_noline)>1000:
+            self.error_label.config(text="Error")#checks to make sure everything is filled in properly and charcter length is not exceeded
+            if len(input_noline)>1000:
+                self.text_error_label.config(text="Over 1,000 characters"+"\n"+"You have "+str(len(input_noline)))
         else:
-            file = open("trip_details.txt", "a")
+            file = open("trip_details.txt", "a")#saves everything to a file
             file.write(values[0] + "|||" + travel_type + "|||" + month.get() + "|||" + input + "\n")
             file.close()
             travel.set('Method of Travel')
             self.lbox.bind(self.lbox.selection_clear(0, END))
             self.descrip_box.delete("1.0", END)
             month.set("January")
+            self.text_error_label.config(text="")
             self.error_label.config(text="Successfully Saved")
 
 
-root = Tk()
+root = Tk()#makes root the main window
 descrip = StringVar()
 travel = StringVar()
 month = StringVar()
 descrip = StringVar()
-app = App(root, month, travel, descrip)
+app = App(root, month, travel, descrip)#all variables amde and passed
 root.title("Travel Log")
-root.geometry("450x250")
-root.minsize(450, 250)
-root.mainloop()
+root.geometry("470x250")
+root.minsize(470, 250)#window specifics
+root.mainloop()#Mainloop makes root the main function that continously runs
 root.destroy()
